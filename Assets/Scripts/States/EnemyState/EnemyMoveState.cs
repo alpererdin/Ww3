e750c66541b3e31e2f6b934a1371decd8d337501
alpererdin <alpererdin636@hotmail.com>
2024-02-05 22:55:23 +0300

    ++

--- Assets/Scripts/States/EnemyState/EnemyMoveState.cs
+++ Assets/Scripts/States/EnemyState/EnemyMoveState.cs
@@ -1,7 +1,29 @@
+using Units;
+using UnityEngine;
 namespace States.EnemyState
 {
-    public class EnemyMoveState
+    public class EnemyMoveState : EnemyBaseState
     {
-        
+        public override EnemyStateType Type => EnemyStateType.Move;
+        private EnemyMain _enemy;
+        public override void EnterState(EnemyMain enemy)
+        {
+            this._enemy = enemy;
+        }
+        public override void UpdateState()
+        {
+            _enemy.transform.position += new Vector3(0, 0, -1) * Time.deltaTime * _enemy.Speed;
+            if (!_enemy.onFight)
+            {
+                Collider[] hitColliders = Physics.OverlapSphere(_enemy.transform.position, _enemy.Range, _enemy.playerLayer);
+                if (hitColliders.Length > 0)
+                {_enemy.onFight = true;
+                    _enemy.SetUnitState(EnemyStateFactory.EnemyFightState());
+                     //true before 
+                }
+            }
+        }
+        public override void FixedUpdate() { }
+        public override void ExitState() { }
     }
 }
--- Assets/Scripts/States/EnemyState/EnemyStateBase.cs
+++ Assets/Scripts/States/EnemyState/EnemyStateBase.cs
@@ -1,7 +1,15 @@
+using Units;
+
 namespace States.EnemyState
 {
-    public class EnemyStateBase
+    public abstract class EnemyStateBase
     {
-        
+        public abstract void EnterState(EnemyMain enemy);
+
+        public abstract void UpdateState();
+
+        public abstract void FixedUpdate();
+
+        public abstract void ExitState();
     }
 }
