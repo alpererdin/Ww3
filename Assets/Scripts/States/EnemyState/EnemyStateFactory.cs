b16f52f2136ee82d05d74bbcc1d308e44f006f01
alpererdin <alpererdin636@hotmail.com>
2024-02-09 07:20:37 +0300

    ++

--- Assets/Scripts/States/EnemyState/EnemyStateFactory.cs
+++ Assets/Scripts/States/EnemyState/EnemyStateFactory.cs
@@ -2,6 +2,18 @@ namespace States.EnemyState
 {
     public class EnemyStateFactory
     {
-        
+        public static EnemyMoveState EnemyMoveState()
+        {
+            return new EnemyMoveState();
+        }
+
+        public static EnemyIdleState EnemyIdleState()
+        {
+            return new EnemyIdleState();
+        }
+        public static EnemyFightState EnemyFightState()
+        {
+            return new EnemyFightState();
+        }
     }
 }
--- Assets/Scripts/Units/EnemyMain.cs
+++ Assets/Scripts/Units/EnemyMain.cs
@@ -1,7 +1,68 @@
+using System;
+using Signals;
+using Stages;
+using States;
+using States.EnemyState;
+using UnityEngine;
 namespace Units
 {
-    public class EnemyMain
+    [Serializable]
+    public abstract class EnemyMain :MonoBehaviour
     {
-        
-    }
+        internal int Speed; 
+        public EnemyBaseState CurrentState;
+        public int ID;
+        public bool onFight = false;
+        public LayerMask playerLayer;
+        public float Range;
+        public ParticleSystem shootParticle;
+        private void OnEnable()
+        {
+            UnitSignals.Instance.SetUnitState += CheckId;
+        }
+        protected void CheckId(int i)
+        {
+            if (ID == i)
+            {
+                SetUnitState(EnemyStateFactory.EnemyMoveState());
+            }
+        }
+        protected abstract void Awake();
+        protected virtual void Start()
+        {
+            if (CurrentState == null)
+            {
+                SetUnitState(EnemyStateFactory.EnemyMoveState());
+            }
+        }
+        public void SetUnitState(EnemyBaseState newState)
+        {
+            CurrentState?.ExitState();
+            CurrentState = newState;
+            CurrentState.EnterState(this);
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (IsTrenchCollider(other))
+            {
+                StageTypeOne stage = other.GetComponent<StageTypeOne>();
+
+                if (CanEnterTrench(stage))
+                {
+                    stage._stageCount++;
+                    stage.HaveEnemy=true;
+                    //UnitSignals.Instance.OnUnitID?.Invoke(ID, BtnColor, stage.pos, other.gameObject);
+                    SetUnitState(EnemyStateFactory.EnemyIdleState());
+                }
+            }
+        }
+        private bool IsTrenchCollider(Collider collider)
+        {
+            return collider.CompareTag("Trench");
+        }
+        private bool CanEnterTrench(StageTypeOne stage)
+        {
+            return stage._stageCount < 3 && !stage.locked;
+        } 
+    } 
 }
