049dd037ef310fc8711391593f727bd815244920
alpererdin <alpererdin636@hotmail.com>
2024-02-13 23:21:12 +0300

    ++

--- Assets/Scripts/Units/EnemySolider.cs
+++ Assets/Scripts/Units/EnemySolider.cs
@@ -1,7 +1,44 @@
+using Signals;
+using States.EnemyState;
+using UnityEngine;
 namespace Units
 {
-    public class EnemySolider
+     
+    public class EnemySolider : EnemyMain
     {
+        public int unitSpeed;
+        public float shootSpeed=.5f;
+        public float bulletSpeed=.5f;
         
+        private bool _soundTick;
+        protected override void Awake()
+        {
+            Speed = unitSpeed;
+            ID = GetInstanceID();
+            var simulatorSpeed = shootParticle.main;
+            var emission = shootParticle.emission;
+            emission.rateOverTimeMultiplier = shootSpeed;
+            simulatorSpeed.simulationSpeed = bulletSpeed;
+        }
+        private void Update()
+        {
+            CurrentState?.UpdateState();
+            if (shootParticle.particleCount>0 && !_soundTick)
+            {
+                // Debug.Log(shoot);
+                _soundTick = true;
+                UnitSignals.Instance.PlaySound?.Invoke(0,transform.position);
+            }
+
+            if (shootParticle.particleCount==0)
+            {
+                _soundTick = false;
+            }
+           
+        }
+        private void FixedUpdate()
+        {
+            CurrentState?.FixedUpdate();
+        }
     }
 }
--- Assets/Scripts/Units/Soldier.cs
+++ Assets/Scripts/Units/Soldier.cs
@@ -1,18 +1,48 @@
+using System;
+using Signals;
+using States;
+using Unity.VisualScripting;
 using UnityEngine;
 namespace Units
 {
     public class Soldier : UnitMain
     {
         public int unitSpeed;
+        public float shootSpeed=.5f;
+        public float bulletSpeed=.5f;
+        
+        private bool _soundTick;
         protected override void Awake()
         {
             Speed = unitSpeed;
             BtnColor = Color.blue;
             ID = GetInstanceID();
+            var simulatorSpeed = shootParticle.main;
+            var emission = shootParticle.emission;
+            emission.rateOverTimeMultiplier = shootSpeed;
+            simulatorSpeed.simulationSpeed = bulletSpeed;
+            
         }
         private void Update()
         {
             CurrentState?.UpdateState();
+           
+            if (shootParticle.particleCount>0 && !_soundTick)
+            {
+               // Debug.Log(shoot);
+               _soundTick = true;
+                UnitSignals.Instance.PlaySound?.Invoke(0,transform.position);
+            }
+
+            if (shootParticle.particleCount==0)
+            {
+                _soundTick = false;
+            }
+            
+        }
+        private void FixedUpdate()
+        {
+            CurrentState?.FixedUpdate();
         }
     }
 }
