8a6656377fcbe3fee27fb8afec793e04cb42556d
alpererdin <alpererdin636@hotmail.com>
2024-02-26 23:59:22 +0300

    ++

---
+++ Assets/Scripts/Data/ScriptableObjects/SoldierData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Data.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class SoldierData : ScriptableObject
+    {
+        
+    }
+}
---
+++ Assets/Scripts/States/EnemyState/EnemyDeathState.cs
@@ -0,0 +1,7 @@
+namespace States.EnemyState
+{
+    public class EnemyDeathState
+    {
+        
+    }
+}
