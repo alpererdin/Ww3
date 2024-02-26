f4eb8991bcd610456bbb08535dc9bbb4eed847f8
alpererdin <alpererdin636@hotmail.com>
2024-02-26 23:59:30 +0300

    ++

--- Assets/Scripts/Data/ScriptableObjects/GameData.cs
+++ Assets/Scripts/Data/ScriptableObjects/GameData.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 namespace Data.ScriptableObjects
 {
-    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    [CreateAssetMenu(fileName = "GameData", menuName = "UnitData/GameData", order = 0)]
     public class GameData : ScriptableObject
     {
+        public float musicValue ;
+        public float effectValue ;
         
     }
 }
--- Assets/Scripts/Data/ScriptableObjects/SoldierData.cs
+++ Assets/Scripts/Data/ScriptableObjects/SoldierData.cs
@@ -1,10 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Data.ScriptableObjects
 {
-    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    [CreateAssetMenu(fileName = "SoldierData", menuName = "UnitData/SoldierData", order = 0)]
     public class SoldierData : ScriptableObject
     {
+        public float baseDamage = 1;
+        public int unitSpeed;
+        public float shootSpeed = 0.5f;
+        public float bulletSpeed = 0.5f;
+        public float shotScatter = 1; // basic = 1 | gunner = 0.1 | machine = 0.5
+        public float unitRange = 25f;
+        public Color btnColor = Color.blue;
+        public float coolDownTime;
         
+        //
+       /* public int currentUpgradeLevel = 0;
+        public float upgradeMultiplier = 1.2f; 
+        public int maxUpgradeLevel = 5; 
+        
+        public float GetCurrentDamage()
+        {
+            return baseDamage * Mathf.Pow(upgradeMultiplier, currentUpgradeLevel);
+        }
+        //*/
+       
+        public Sprite imgg;
+      
+
     }
 }
